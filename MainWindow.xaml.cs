e82885da4933601941b33b6b7b622da9583628a2
k-pylinskyi <k.pylinskyi@gmail.com>
2020-11-02 12:35:03 +0100

    Commenting project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace TicTacToe
         public MainWindow()
         {
             InitializeComponent();
-
+            //Game start method
             GameStart();
         }
         #endregion
@@ -28,7 +28,7 @@ namespace TicTacToe
         private MarkType[] markResult;
 
         /// <summary>
-        /// True if it is player 1's turn (X) or player 2's turn (O)
+        /// True if it is player 1's turn (X) or false if it is player 2's turn (O)
         /// </summary>
         private bool playerTurn;
 
@@ -77,17 +77,20 @@ namespace TicTacToe
         /// <param name="e">event</param>
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
+            // Check if game is not ended
             if (gameEnded)
             {
+                //if game ended start new game
                 GameStart();
                 return;
             }
 
             var button = (Button)sender;
-
+            //get the column of button
             var column = Grid.GetColumn(button);
+            //get the row of button
             var row = Grid.GetRow(button);
-
+            //creating index for button
             var index = column + (row * 3);
 
             // Don't do anything if the cell already has a value in it
@@ -115,10 +118,14 @@ namespace TicTacToe
         private void CheckForWiner()
         {
             #region Horizontal wins
+            // true if column of index zero have the same type cells
             bool horizontalZero = ((markResult[0] & markResult[1] & markResult[2]) == markResult[0]);
+            // true if column of index one have the same type cells
             bool horizontalOne = ((markResult[3] & markResult[4] & markResult[5]) == markResult[3]);
+            // true if column of index two have the same type cells
             bool horizontalTwo = ((markResult[6] & markResult[7] & markResult[8]) == markResult[6]);
 
+            // if horizontal zero column is same type and not free
             if (markResult[0] != MarkType.Free && horizontalZero)
             {
                 // Game ends
@@ -127,6 +134,7 @@ namespace TicTacToe
                 // Highlight wining cells
                 Button0_0.Background = Button1_0.Background = Button2_0.Background = Brushes.DarkCyan;
             }
+            // if horizontal one column is same type and not free
             else if (markResult[3] != MarkType.Free && horizontalOne)
             {
                 // Game ends
@@ -135,6 +143,7 @@ namespace TicTacToe
                 // Highlight wining cells
                 Button0_1.Background = Button1_1.Background = Button2_1.Background = Brushes.DarkCyan;
             }
+            // if horizontal two column is same type and not free
             else if (markResult[6] != MarkType.Free && horizontalTwo)
             {
                 // Game ends
@@ -147,10 +156,14 @@ namespace TicTacToe
             #endregion
 
             #region Vertical wins
+            // true if row of index zero have the same type cells
             bool verticalZero = ((markResult[0] & markResult[3] & markResult[6]) == markResult[0]);
+            // true if row of index one have the same type cells
             bool verticalOne = ((markResult[1] & markResult[4] & markResult[7]) == markResult[1]);
+            // true if row of index two have the same type cells
             bool verticalTwo = ((markResult[2] & markResult[5] & markResult[8]) == markResult[2]);
 
+            // if vertical zero row is same type and not free
             if (markResult[0] != MarkType.Free && verticalZero)
             {
                 // Game ends
@@ -159,6 +172,7 @@ namespace TicTacToe
                 // Highlight wining cells
                 Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.DarkCyan;
             }
+            // if vertical one row is same type and not free
             else if (markResult[1] != MarkType.Free && verticalOne)
             {
                 // Game ends
@@ -167,6 +181,7 @@ namespace TicTacToe
                 // Highlight wining cells
                 Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.DarkCyan;
             }
+            // if vertical two row is same type and not free
             else if (markResult[2] != MarkType.Free && verticalTwo)
             {
                 // Game ends
@@ -178,9 +193,12 @@ namespace TicTacToe
             #endregion
 
             #region Cross wins
+            // true if cross of game field (type 1) have the same type cells
             bool CrossOne = ((markResult[0] & markResult[4] & markResult[8]) == markResult[4]);
+            // true if cross of game field (type 2) have the same type cells
             bool CrossTwo = ((markResult[2] & markResult[4] & markResult[6]) == markResult[4]);
 
+            // if cross of type one is same type and not free
             if (markResult[4] != MarkType.Free && CrossOne)
             {
                 // Game ends
@@ -189,6 +207,7 @@ namespace TicTacToe
                 // Highlight wining cells
                 Button0_0.Background = Button1_1.Background = Button2_2.Background = Brushes.DarkCyan;
             }
+            // if cross of type two is same type and not free
             else if (markResult[4] != MarkType.Free && CrossTwo)
             {
                 // Game ends
@@ -200,6 +219,7 @@ namespace TicTacToe
             #endregion
 
             #region No winers
+            // if there is no free cells on gamefield
             if (!markResult.Any(cell => cell == MarkType.Free))
             {
                 // Game ends
@@ -213,12 +233,6 @@ namespace TicTacToe
                 });
             }
             #endregion
-
-
-
-
-
-
         }
     }
 }
